630ab1cfdc37e6960207c6b79d284999ff59b8bc
Amisha Bhanage <amisha_bhanage@yahoo.com>
2017-11-18 17:07:27 -0800

    Add files via upload

--- DSE203/Final/ML View.sql
+++ DSE203/Final/ML View.sql
@@ -24,15 +24,21 @@ CREATE OR REPLACE VIEW salesaggregate AS
            FROM sales s
           WHERE ((s.year::text || lpad(s.month::text, 2, '0'::text))::integer) >= COALESCE(curr.l12myyyymm, prev.l12myyyymm) AND ((s.year::text || lpad(s.month::text, 2, '0'::text))::integer) < COALESCE(curr.yyyymm, prev.nmyyyymm) AND s.nodeid = COALESCE(curr.nodeid, prev.nodeid)
           GROUP BY s.nodeid), 0::numeric) AS l12m_total_sales_price,
+    COALESCE(rev.numreviews, 0::numeric)   AS   numreviews_by_period,
     COALESCE(rev.avgrating, 0::numeric) AS avgrating_by_period,
+    COALESCE(( SELECT count(r.reviewid) AS numreviews
+           FROM reviews r
+             LEFT JOIN products p ON p.asin = r.asin
+          WHERE COALESCE(curr.nodeid, prev.nodeid) = COALESCE(p.nodeid::bigint, 0::bigint)
+          GROUP BY (COALESCE(p.nodeid::bigint, 0::bigint))), 0::numeric) AS numreviews ,
     COALESCE(( SELECT round(avg(r.overall), 2) AS avgrating
            FROM reviews r
              LEFT JOIN products p ON p.asin = r.asin
           WHERE COALESCE(curr.nodeid, prev.nodeid) = COALESCE(p.nodeid::bigint, 0::bigint)
-          GROUP BY (COALESCE(p.nodeid::bigint, 0::bigint))), 0::numeric) AS avgrating
+          GROUP BY (COALESCE(p.nodeid::bigint, 0::bigint))), 0::numeric) AS avgrating      
    FROM ( SELECT s.nodeid,
             s.month,
-        --    s.monthabbr,
+            s.monthabbr,
             s.year,
             (s.year::text || lpad(s.month::text, 2, '0'::text))::integer AS yyyymm,
             s.season,
@@ -45,8 +51,7 @@ CREATE OR REPLACE VIEW salesaggregate AS
             sum(s.numunits) AS total_sales_volume,
             sum(s.totalprice::numeric) AS total_sales_price
            FROM sales s
-          GROUP BY s.nodeid, s.month, --s.monthabbr,
-         s.year, ((s.year::text || lpad(s.month::text, 2, '0'::text))::integer), s.season, s.holidayseason, (date_part('month'::text, (date_trunc('month'::text, s.orderdate::timestamp with time zone) - '1 mon'::interval)::date)), (date_part('year'::text, (date_trunc('month'::text, s.orderdate::timestamp with time zone) - '1 mon'::interval)::date)), ((date_part('year'::text, date_trunc('month'::text, s.orderdate::timestamp with time zone) - '3 mons'::interval)::text || lpad(date_part('month'::text, date_trunc('month'::text, s.orderdate::timestamp with time zone) - '3 mons'::interval)::text, 2, '0'::text))::integer), ((date_part('year'::text, date_trunc('month'::text, s.orderdate::timestamp with time zone) - '1 year'::interval)::text || lpad(date_part('month'::text, date_trunc('month'::text, s.orderdate::timestamp with time zone) - '1 year'::interval)::text, 2, '0'::text))::integer)) curr
+          GROUP BY s.nodeid, s.month, s.monthabbr, s.year, ((s.year::text || lpad(s.month::text, 2, '0'::text))::integer), s.season, s.holidayseason, (date_part('month'::text, (date_trunc('month'::text, s.orderdate::timestamp with time zone) - '1 mon'::interval)::date)), (date_part('year'::text, (date_trunc('month'::text, s.orderdate::timestamp with time zone) - '1 mon'::interval)::date)), ((date_part('year'::text, date_trunc('month'::text, s.orderdate::timestamp with time zone) - '3 mons'::interval)::text || lpad(date_part('month'::text, date_trunc('month'::text, s.orderdate::timestamp with time zone) - '3 mons'::interval)::text, 2, '0'::text))::integer), ((date_part('year'::text, date_trunc('month'::text, s.orderdate::timestamp with time zone) - '1 year'::interval)::text || lpad(date_part('month'::text, date_trunc('month'::text, s.orderdate::timestamp with time zone) - '1 year'::interval)::text, 2, '0'::text))::integer)) curr
      FULL JOIN ( SELECT s.nodeid,
             s.month,
             s.year,
@@ -62,9 +67,11 @@ CREATE OR REPLACE VIEW salesaggregate AS
      LEFT JOIN ( SELECT COALESCE(p.nodeid::bigint, 0::bigint) AS nodeid,
             c.month,
             c.year,
+            count(r.reviewid) AS numreviews,    
             round(avg(r.overall), 2) AS avgrating
            FROM reviews r
              LEFT JOIN products p ON p.asin = r.asin
              JOIN calendar c ON c.date = r.reviewtime
           GROUP BY (COALESCE(p.nodeid::bigint, 0::bigint)), c.month, c.year) rev ON COALESCE(curr.nodeid, prev.nodeid) = rev.nodeid AND COALESCE(curr.month::double precision, prev.nm) = rev.month::double precision AND COALESCE(curr.year::double precision, prev.nmy) = rev.year::double precision;
+
 
